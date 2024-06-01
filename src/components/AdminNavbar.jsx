11ebe2187b668734a2d83996cd33f6db84039ae4
Gokuktkn <120592109+Gokuktkn@users.noreply.github.com>
2024-06-01 21:35:27 +0700

    Chỉnh adminNavbar

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -5,7 +5,6 @@ function AdminNavbar() {
     return (
         <div className='admin-navbar-content'>
             <NavLink to={"/admin/create"} className="nav nav-top">Thêm sản phẩm</NavLink>
-            <NavLink to={"/admin/update"} className="nav">Cập nhật sản phẩm</NavLink>
             <NavLink to={"/admin/management"} className="nav">Quản lý sản phẩm</NavLink>
             <NavLink to={"/admin/feedback"} className="nav nav-bot">Ý kiến khách hàng</NavLink>
         </div>
